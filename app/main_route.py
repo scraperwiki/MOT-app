0bd79fbdceca607cdb9d5135431257b4b15bbb58
piusokoh_scraperwiki <pius@scraperwiki.com>
2015-06-30 12:16:50 +0100

    variables visible in flask

--- app/main_route.py
+++ app/main_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect
 #from app import app
 app = Flask(__name__, static_url_path = "/static")
 
@@ -9,20 +9,16 @@ app = Flask(__name__, static_url_path = "/static")
 def root():
     return app.send_static_file("car-search.html")
 
-@app.route('/<make>')
-def visit_make(make):
-    recognised_makes = ['ford', 'benz', 'audi', 'volkswagen']
-    make_data = {'ford': 1,
-                 'benz': 2,
-                 'audi': 3,
-                 'volkswagen': 4}
-    # Read CSV
-    # Look up make from CSV
-    if make.lower() in recognised_makes:
-        return 'I recognise {}. {} '.format(make_data[make.lower()], make)
-    else:
-        return 'I do not recognise {}. {} '.format(make)
-    return 'Error in input!'
+@app.route('/', methods=['POST'])
+def navigate():
+    return redirect("/{}/{}".format(request.form['make'], request.form['model']))
+
+
+@app.route('/<make>/<model>')
+def visit_make(make, model):
+    """obtain the values chosen by the user for make and model..."""
+    
+    return jsonify(result=make + model)    
 
 
 if __name__ == '__main__':
