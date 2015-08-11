7c427a716bc4b139d42a646de7c049760a57d5f9
Dragon Dave McKee <dragon@scraperwiki.com>
2015-08-11 17:22:24 +0100

    autopep8

--- app/main_route.py
+++ app/main_route.py
@@ -22,10 +22,10 @@ data_dict_rates = {}
 def parse_file():
     data_dict_builder = {}
     global data_dict
-    Bigrecord = namedtuple("Bigrecord", 
+    Bigrecord = namedtuple("Bigrecord",
         "make model year level1 level2 level3 modelcount")
     def make_record_level2(line):
-        (make, model, year,  
+        (make, model, year,
             level1, level2, level3, modelcount) = line
         return Bigrecord(make, model, year, level1, level2, level3,
          int(modelcount))
@@ -36,16 +36,16 @@ def parse_file():
         records = [make_record_level2(r) for r in records]
 
     for r in records:
-        data_dict_builder.setdefault(r.make, {}).setdefault(r.model, 
-            {}).setdefault(r.year, {}).setdefault(r.level1, 
+        data_dict_builder.setdefault(r.make, {}).setdefault(r.model,
+            {}).setdefault(r.year, {}).setdefault(r.level1,
             []).append((r.level2, r.level3, r.modelcount))
-        
+
         data_dict = data_dict_builder
 
 def parse_file_rates():
     data_dict_builder = {}
     global data_dict_rates
-    Bigrecord = namedtuple("Bigrecord", 
+    Bigrecord = namedtuple("Bigrecord",
         "make model year testresult modelcount")
     def make_record(line):
         (make, model, year, testresult, modelcount) = line
@@ -57,12 +57,12 @@ def parse_file_rates():
         records = [make_record(r) for r in records]
 
     for r in records:
-        data_dict_builder.setdefault(r.make, {}).setdefault(r.model, 
+        data_dict_builder.setdefault(r.make, {}).setdefault(r.model,
             {}).setdefault(r.year, {})[r.testresult] = r.modelcount
-                   
+
 
     data_dict_rates = data_dict_builder
-    
+
 
 ######################### utility functions ###################################
 
@@ -88,7 +88,7 @@ def select_make_model_rate(make, model, year=None):
         return data_dict_rates[make][model][year]
 
 def calculate_pass_rate_year(dictionary):
-    
+
     passes, fails = dictionary.get("P", 0), dictionary.get("F", 0)
     total = passes + fails
     rate = round((100*passes / total), 1)
@@ -96,7 +96,7 @@ def calculate_pass_rate_year(dictionary):
 
 def calculate_pass_rate_all(dictionary):
     passfail_counts = {"P": 0, "F": 0}
-    
+
     for year, passfail in dictionary.items():
         passfail_counts["P"] += passfail.get("P", 0)
         passfail_counts["F"] += passfail.get("F", 0)
@@ -121,8 +121,8 @@ def extract_level1(dictionary):
         for level1, bigrecords in levels.items():
         	running_total = level1_dictionary.get(level1, 0)
         	failure_count = sum(bigrecord[-1] for bigrecord in bigrecords)
-        	level1_dictionary[level1] = running_total + failure_count 
-    
+        	level1_dictionary[level1] = running_total + failure_count
+
     return level1_dictionary
 
 
@@ -159,40 +159,40 @@ def analyse_level2(mylevel1, dictionary):
                 for bigrecord in bigrecords:
                     desc = bigrecord[0] + ': ' + bigrecord[1]
                     running_total = level2_dict.get(desc, 0)
-                    level2_dict[desc] = running_total + bigrecord[-1]   
+                    level2_dict[desc] = running_total + bigrecord[-1]
     return level2_dict
 
 ################### computational app routing #################################
 @app.route('/make', methods=['GET', 'POST'])
-def getModel(): 
+def getModel():
     make = request.json['make']
-    models = list(data_dict_rates[make].keys()) 
-    models = json.dumps(models)  
+    models = list(data_dict_rates[make].keys())
+    models = json.dumps(models)
     return models
 
 @app.route('/make/model', methods=['GET', 'POST'])
-def getYear(): 
+def getYear():
     make = request.json['make']
     model = request.json['model']
-    years = list(data_dict_rates[make][model].keys()) 
-    years = json.dumps(years)  
+    years = list(data_dict_rates[make][model].keys())
+    years = json.dumps(years)
     return years
 
 ##################### navigational app routing ################################
 
 @app.route('/')
 def index():
-    makes = list(data_dict_rates.keys())    
+    makes = list(data_dict_rates.keys())
     return render_template("index.html", make=makes)
 
 @app.route('/', methods=['POST'])
 def navigate():
     if request.form['year']=='Select a year':
         if request.form['submit-button']=='Display Top Faults':
-            return redirect("/FAULTS/{}/{}".format(request.form['make'], 
+            return redirect("/FAULTS/{}/{}".format(request.form['make'],
                 request.form['model']))
         else:
-            return redirect("/PASS/{}/{}".format(request.form['make'], 
+            return redirect("/PASS/{}/{}".format(request.form['make'],
                 request.form['model']))
 
     else:
@@ -200,23 +200,23 @@ def navigate():
             return redirect("/{}/FAULTS/{}/{}".format(request.form['year'],
                 request.form['make'], request.form['model']))
         else:
-            return redirect("/PASS/{}/{}/{}".format(request.form['make'], 
+            return redirect("/PASS/{}/{}/{}".format(request.form['make'],
                 request.form['model'], request.form['year']))
 
 ######################### pass navigations ##################################
 
 @app.route('/PASS/<make>/<model>')
-def pass_vehicle_allyears(make, model):    
+def pass_vehicle_allyears(make, model):
     passes, fails, rate = calculate_pass_rate_all(
-        select_make_model_rate(make, model))     
-    return render_template("passrate.html", make=make, model=model, 
+        select_make_model_rate(make, model))
+    return render_template("passrate.html", make=make, model=model,
         count_fail=fails, count_pass=passes, rate=rate)
 
 @app.route('/PASS/<make>/<model>/<year>')
-def pass_vehicle_byyear(make, model, year):    
+def pass_vehicle_byyear(make, model, year):
     passes, fails, rate = calculate_pass_rate_year(
         select_make_model_rate(make, model, year))
-    return render_template("passrateyear.html", make=make, model=model, 
+    return render_template("passrateyear.html", make=make, model=model,
         year=year, count_fail=fails, count_pass=passes, rate=rate)
 
 ######################### faults navigations #############################
@@ -226,10 +226,10 @@ def pass_vehicle_byyear(make, model, year):
 def visit_vehicle_level1(make, model):
     """obtain the values chosen by the user for make and model..."""
     level1 = extract_level1(select_make_model(make, model))
-    level1_tuples = analyse_level1(level1)    
+    level1_tuples = analyse_level1(level1)
     results_dictionary, sum_of_counts = utilities.create_results_dictionary(level1_tuples)
     fig = utilities.results_graph(results_dictionary)
-    return render_template('resultlevel1.html', results=results_dictionary, 
+    return render_template('resultlevel1.html', results=results_dictionary,
         make=make, model=model, total=sum_of_counts, fig=fig)
 
 @app.route('/<year>/FAULTS/<make>/<model>')
@@ -241,32 +241,32 @@ def visit_vehicle_level1_byyear(make, model, year):
     print(level1_tuples)
     results_dictionary, sum_of_counts = utilities.create_results_dictionary(level1_tuples)
     fig = utilities.results_graph(results_dictionary)
-    
-    return render_template('resultlevel1_year.html', results=results_dictionary, 
+
+    return render_template('resultlevel1_year.html', results=results_dictionary,
         make=make, model=model, year=year, total=sum_of_counts, fig=fig)
 
 ############################### level 2 ##################################
 @app.route('/FAULTS/<make>/<model>/<level1>')
 def visit_vehicle_level2(make, model, level1):
-    level2_tuples = analyse_level1(select_level2(make, model, level1)) 
+    level2_tuples = analyse_level1(select_level2(make, model, level1))
 
     results_dictionary, sum_of_counts = utilities.create_results_dictionary(level2_tuples)
 
     fig = utilities.results_graph(results_dictionary)
-    return render_template('resultlevel2.html', results=results_dictionary, 
+    return render_template('resultlevel2.html', results=results_dictionary,
         make=make, model=model, total=sum_of_counts, level1=level1, fig=fig)
-    
+
 @app.route('/<year>/FAULTS/<make>/<model>/<level1>')
 def visit_vehicle_level2_byyear(make, model, level1, year):
     level2_tuples = select_level2(make, model, level1, year)
-    
+
     results_dictionary, sum_of_counts = utilities.create_results_dictionary(level2_tuples)
 
     fig = utilities.results_graph(results_dictionary)
-    
-    return render_template('resultlevel2_year.html', results=results_dictionary, 
+
+    return render_template('resultlevel2_year.html', results=results_dictionary,
         make=make, model=model, year=year, total=sum_of_counts, level1=level1, fig=fig)
-    
+
 ########################### run the app #######################################
 if __name__ == '__main__':
     parse_file()
